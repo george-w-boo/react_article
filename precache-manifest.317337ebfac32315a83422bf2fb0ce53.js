57f13256968debc6260ac4682edcdb318d65dd97
Mykhailo Ostapysko <mp4clips@gmail.com>
2021-08-31 13:23:59 +0300

    Updates

---
+++ precache-manifest.317337ebfac32315a83422bf2fb0ce53.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c9d4dbed770306dc2d1d55236f981f87",
+    "url": "./index.html"
+  },
+  {
+    "revision": "61e7aeb5a2e3db6ff37f",
+    "url": "./static/js/2.d1ce54ea.chunk.js"
+  },
+  {
+    "revision": "570d362d673dab785e62d2b8563e1118",
+    "url": "./static/js/2.d1ce54ea.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "6f62cca47b5544e17182",
+    "url": "./static/js/main.16a1168e.chunk.js"
+  },
+  {
+    "revision": "994f72ec58672ee8392d",
+    "url": "./static/js/runtime-main.a5fa282b.js"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "./precache-manifest.575c894ccaaaec018efa3e6d1c5de3fa.js"
+  "./precache-manifest.317337ebfac32315a83422bf2fb0ce53.js"
 );
 
 self.addEventListener('message', (event) => {
